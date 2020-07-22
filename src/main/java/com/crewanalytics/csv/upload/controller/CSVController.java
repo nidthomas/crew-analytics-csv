e1310373f9d3505c322f0a8faf70988e5ec62608
nid1992 <nithinthomas2011@gmail.com>
2020-07-22 23:14:11 +0530

    Added reset feature
    feature to delete all entries from db

--- src/main/java/com/crewanalytics/csv/upload/controller/CSVController.java
+++ src/main/java/com/crewanalytics/csv/upload/controller/CSVController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -62,6 +59,22 @@ public class CSVController {
             }
             return new ResponseEntity<>(crews, HttpStatus.OK);
         }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/reset")
+    public ResponseEntity<ResponseMessage> deleteAll(){
+
+        try {
+                if(!fileService.getAllCrew().isEmpty()) {
+                    fileService.deleteAll();
+                    log.warn("Deleted all entries");
+                    return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("Deleted all entries!!"));
+                }
+                log.warn("Nothing to delete");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage("No entries to delete!!"));
+            } catch (Exception exception) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- src/main/java/com/crewanalytics/csv/upload/repository/CrewRepository.java
+++ src/main/java/com/crewanalytics/csv/upload/repository/CrewRepository.java
@@ -2,6 +2,8 @@ package com.crewanalytics.csv.upload.repository;
 
 import com.crewanalytics.csv.upload.model.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CrewRepository extends JpaRepository<Crew,Integer> {
 }
