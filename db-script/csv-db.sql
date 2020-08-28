f98105d5e4b074d5c31c8c46cbe5eea903ffbf66
nid1992 <nithinthomas2011@gmail.com>
2020-08-28 15:45:28 +0530

    added new csv, db script changes

--- db-script/csv-db.sql
+++ db-script/csv-db.sql
@@ -1,3 +1,4 @@
 create database crew_analytics;
 use crew_analytics;
 create table crew (Crew_Utilized float(4,2),Planned int(2),Crew_Rank varchar(20),Base_Station varchar(20));
+alter table crew add column home_ratio float GENERATED ALWAYS AS (utilized_airport_standby/departures) stored;
